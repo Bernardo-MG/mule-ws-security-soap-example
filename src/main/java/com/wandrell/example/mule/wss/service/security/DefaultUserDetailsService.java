2873e293855e6c08d7ef7e31c255624d790ead3f
Bernardo Martínez Garrido <programming@wandrell.com>
2016-06-13 10:21:49 +0200

    Corrected password callback handler for keystores.

--- src/main/java/com/wandrell/example/mule/wss/service/security/DefaultUserDetailsService.java
+++ src/main/java/com/wandrell/example/mule/wss/service/security/DefaultUserDetailsService.java
@@ -78,6 +78,12 @@ public final class DefaultUserDetailsService implements UserDetailsService {
             authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
 
             user = new User(username, "myPassword", authorities);
+        } else if ("swss-cert".equalsIgnoreCase(username)) {
+            // User for password-based security
+            authorities = new LinkedList<SimpleGrantedAuthority>();
+            authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+
+            user = new User(username, "123456", authorities);
         } else {
             // User not found
             LOGGER.debug(
